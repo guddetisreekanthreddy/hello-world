c8a8cf7dab64ca5060890453ca651851ee9583df
guddetisreekanthreddy <67550681+guddetisreekanthreddy@users.noreply.github.com>
2020-08-23 16:30:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello main method ...");
     System.out.print("Current Time in milliseconds = ");
-      System.out.println(System.currentTimeMillis());
+    System.out.println(System.currentTimeMillis());
   }
 }
